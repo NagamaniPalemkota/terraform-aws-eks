7ea43c2b28ff175b8d90d0a2d55e2cc76e6e8b80
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-19 16:10:56 +0530

    committing cluster

--- 04-eks/data.tf
+++ 04-eks/data.tf
@@ -10,6 +10,6 @@ data "aws_ssm_parameter" "cluster_sg_id" {
     name = "/${var.project_name}/${var.environment}/cluster_sg_id"
 }
 
-data "aws_ssm_parameter" "node_sg_id_sg_id" {
+data "aws_ssm_parameter" "node_sg_id" {
     name = "/${var.project_name}/${var.environment}/node_sg_id"
 }

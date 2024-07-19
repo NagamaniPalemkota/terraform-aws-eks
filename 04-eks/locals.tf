963c65f6d735fbe2d7f7a095147fcc6824f205fd
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-19 16:06:57 +0530

    committing cluster

--- 04-eks/locals.tf
+++ 04-eks/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    private_subnet_id =  data.aws_ssm_parameter.private_subnet_ids.value
+    private_subnet_ids =  data.aws_ssm_parameter.private_subnet_ids.value
     vpc_id = data.aws_ssm_parameter.ssm_vpc_info.value
     cluster_sg_id = data.aws_ssm_parameter.cluster_sg_id.value
     node_sg_id = data.aws_ssm_parameter.node_sg_id.value
